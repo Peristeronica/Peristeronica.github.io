8731532ecfac78e25767dfca64becce8e4a43689
Peristeronica <enjoys1152@gmail.com>
2025-07-21 18:59:24 +0900

    セミナーガーデン追加

--- inside_script.js
+++ inside_script.js
@@ -53,7 +53,8 @@ document.addEventListener('DOMContentLoaded', () => {
         '<script type="application/javascript" src="https://embed.nicovideo.jp/watch/sm44958180/script?w=720&h=480"></script><noscript><a href="https://www.nicovideo.jp/watch/sm44958180">乳出会／はみ出タル風キ委員ング</a></noscript>',
         '<script type="application/javascript" src="https://embed.nicovideo.jp/watch/sm42496640/script?w=720&h=480"></script><noscript><a href="https://www.nicovideo.jp/watch/sm42496640">あっはは…確かに昨日『暑くて歪みそう』とは言ったけどさ～…</a></noscript>',
         '<script type="application/javascript" src="https://embed.nicovideo.jp/watch/sm45068051/script?w=720&h=480"></script><noscript><a href="https://www.nicovideo.jp/watch/sm45068051">七味唐辛子を販売する宇沢レイサ</a></noscript>',
-        '<script type="application/javascript" src="https://embed.nicovideo.jp/watch/sm44138206/script?w=720&h=480"></script><noscript><a href="https://www.nicovideo.jp/watch/sm44138206">まっカなリフォ</a></noscript>'
+        '<script type="application/javascript" src="https://embed.nicovideo.jp/watch/sm44138206/script?w=720&h=480"></script><noscript><a href="https://www.nicovideo.jp/watch/sm44138206">まっカなリフォ</a></noscript>',
+        '<script type="application/javascript" src="https://embed.nicovideo.jp/watch/sm44722580/script?w=720&h=480"></script><noscript><a href="https://www.nicovideo.jp/watch/sm44722580">セミナーガーデン</a></noscript>'
     ];
 
     const videoContainer = document.getElementById('video-container');
